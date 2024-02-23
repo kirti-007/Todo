b5390afb2b63a4d306f86235c9a85ddc282902d4
Kirti Mahapatra <kirtimahapatra07@gmail.com>
2024-02-23 21:31:40 +0530

    Added

--- ToDo-MVC/ToDoList/Custom_Scripts/BuildTable.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $.ajax({
-        url: '/ToDoes/BuildTodoTable',
-        success: function (result) {
-            $('#tableDiv').html(result);
-        }
-    });
-
-}); 
